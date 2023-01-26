53ba1a41abefbc09d807c772fd297c2788ebac9f
knottem <59744487+knottem@users.noreply.github.com>
2023-01-26 19:25:17 +0100

    Update PrintHelp.java

--- src/main/java/se/na/shoedatabase/view/PrintHelp.java
+++ src/main/java/se/na/shoedatabase/view/PrintHelp.java
@@ -1,5 +1,6 @@
 package se.na.shoedatabase.view;
 
+import se.na.shoedatabase.model.shoe.Category;
 import se.na.shoedatabase.model.shoe.Shoe;
 import java.util.ArrayList;
 
@@ -11,12 +12,10 @@ public class PrintHelp {
         System.out.printf("----------------------------------------------------------------------------%n");
         shoes.stream().filter(s -> s.getQuantity() > 0).forEach(s -> {
             StringBuilder categories = new StringBuilder();
-            for (int i = 0; i < s.getCategories().size(); i++) {
-                categories.append(s.getCategories().get(i).getCategoryname()).append(", ");
-                if(i == s.getCategories().size()-1){
-                    categories.setLength((categories.length()-2));
-                }
+            for(Category category : s.getCategories()){
+                categories.append(category.getCategoryname()).append(", ");
             }
+            categories.setLength((categories.length()-2));
             System.out.printf("| %-2s | %-9s | %7s | %6s | %5s | %-20s | %4s |%n",
                     s.getId(), s.getBrand(), s.getSize(), s.getColor(), s.getQuantity(), categories, s.getPrice());
         });
